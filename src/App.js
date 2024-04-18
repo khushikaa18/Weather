4dd4fa4bb70a28b04876d915d4c5a08a1f2a8b3b
khushikaa18 <154812853+khushikaa18@users.noreply.github.com>
2024-04-18 20:07:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import WeatherApp from './Components/WeatherApp/WeatherApp';
+function App() {
+  return (
+    <div className="App">
+      <WeatherApp/>
+    </div>
+  );
+}
+
+export default App;
